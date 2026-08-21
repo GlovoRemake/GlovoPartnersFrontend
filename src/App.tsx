126a6cac178753734ed314b75b0e055ef674f3ab
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-21 20:39:09 +0300

    add react router and change style chadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
 import './App.css'
-import {useEffect} from "react";
-import {useLoginMutation, useSendRequestCompanyMutation} from "@/services/apiPartner.ts";
+// import {useEffect} from "react";
+// import {useLoginMutation, useSendRequestCompanyMutation} from "@/services/apiPartner.ts";
+import { Route, Routes } from 'react-router';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-    const [testLogin] = useLoginMutation();
-    const [send] = useSendRequestCompanyMutation();
+    // const [testLogin] = useLoginMutation();
+    // const [send] = useSendRequestCompanyMutation();
 
-    useEffect(() => {
-        const test = async () => {
-            try {
-                await testLogin({
-                    email: "rocafig361@jobraux.com",
-                    password: "123123123",
-                });
+    // useEffect(() => {
+    //     const test = async () => {
+    //         try {
+    //             await testLogin({
+    //                 email: "rocafig361@jobraux.com",
+    //                 password: "123123123",
+    //             });
 
-                await send({
-                    name: "123123",
-                    description: "123123"
-                });
-            } catch (e) {
-                console.error(e);
-            }
-        }
+    //             await send({
+    //                 name: "123123",
+    //                 description: "123123"
+    //             });
+    //         } catch (e) {
+    //             console.error(e);
+    //         }
+    //     }
 
 
-        test();
-    }, []);
+    //     test();
+    // }, []);
 
   return (
     <>
-        <div className={"bg-black w-full h-screen"}>
+        <Routes>
+            <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        {/* <div className={"bg-black w-full h-screen"}>
             <h1 className={"text-4xl text-green-500"}>Get started</h1>
-        </div>
+        </div> */}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { store } from './store/index.ts'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
   </Provider>,
 )
